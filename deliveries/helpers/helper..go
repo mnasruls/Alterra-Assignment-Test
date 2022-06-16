86b167fbc64771f8ec7ebff17cb8b0a0af07ba85
nasrul0807 <nasrulmuhammad748@gmail.com>
2022-06-16 23:10:23 +0700

    helper: membuat function untuk hash password dan check hash password

--- deliveries/helpers/helper..go
+++ deliveries/helpers/helper..go
@@ -1,1 +1,26 @@
 package helpers
+
+import (
+	"alterra/entities/web"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func MakeErrorResponse(status string, code int, err string, links map[string]string) web.ErrorResponse {
+	return web.ErrorResponse{
+		Status: status,
+		Code:   code,
+		Error:  err,
+		Links:  links,
+	}
+}
