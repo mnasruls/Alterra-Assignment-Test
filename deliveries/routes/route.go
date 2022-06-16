45e8b66d02d1df1b1f11d16052230c87e84ba669
nasrul0807 <nasrulmuhammad748@gmail.com>
2022-06-17 01:04:07 +0700

    route: membuat router create, update dan delete user

--- deliveries/routes/route.go
+++ deliveries/routes/route.go
@@ -1,1 +1,15 @@
 package routes
+
+import (
+	"alterra/deliveries/handlers"
+	"alterra/deliveries/middlewares"
+
+	"github.com/labstack/echo/v4"
+)
+
+func RegisterAdminRoute(e *echo.Echo, userHandler *handlers.AdminHandler) {
+	group := e.Group("/api/admins", middlewares.JWTMiddleware())
+	group.POST("", userHandler.Create)       // Registration
+	group.PUT("/:id", userHandler.Update)    //Edit
+	group.DELETE("/:id", userHandler.Delete) //Delete
+}
