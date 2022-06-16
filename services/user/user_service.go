a86d295ecfc8ad80b278ad2ecb350317f6b0baa0
nasrul0807 <nasrulmuhammad748@gmail.com>
2022-06-17 00:05:50 +0700

    service: membuat service untuk CRUD user

--- services/user/user_service.go
+++ services/user/user_service.go
@@ -71,3 +71,105 @@ func (service UserService) CreateUser(userRequest entities.CreateUserRequest) (e
 	}
 	return authRes, nil
 }
+
+/*
+ * User Service - FindAll
+ * -------------------------------
+ * Mencari semua user
+ */
+func (service UserService) FindAll(limit, page int, filters []map[string]string, sorts []map[string]interface{}) ([]entities.UserResponse, error) {
+
+	offset := (page - 1) * limit
+
+	usersRes := []entities.UserResponse{}
+
+	// Mengambil data user dari repository
+	users, err := service.userRepo.FindAllUser(limit, offset, filters, sorts)
+	if err != nil {
+		return []entities.UserResponse{}, err
+	}
+
+	// proses menjadi user response
+	copier.Copy(&usersRes, &users)
+
+	return usersRes, err
+}
+
+/*
+ * User Service - Find
+ * -------------------------------
+ * Mencari user berdasarkan ID
+ */
+func (service UserService) Find(id int) (entities.UserResponse, error) {
+
+	// Mengambil data user dari repository
+	user, err := service.userRepo.Find(id)
+	if err != nil {
+		return entities.UserResponse{}, err
+	}
+
+	// proses menjadi user response
+	userRes := entities.UserResponse{}
+	copier.Copy(&userRes, &user)
+
+	return userRes, err
+}
+
+/*
+ * User Service - FindBy
+ * -------------------------------
+ * Mencari user berdasarkan column dan value
+ */
+func (service UserService) FindBy(field string, value string) (entities.UserResponse, error) {
+
+	// Mengambil data user dari repository
+	user, err := service.userRepo.FindBy(field, value)
+	if err != nil {
+		return entities.UserResponse{}, err
+	}
+
+	// proses menjadi user response
+	userRes := entities.UserResponse{}
+	copier.Copy(&userRes, &user)
+
+	return userRes, err
+}
+
+func (service UserService) UpdateUser(userRequest entities.UpdateUserRequest, id int) (entities.UserResponse, error) {
+
+	// Get user by ID via repository
+	user, err := service.userRepo.Find(id)
+	if err != nil {
+		return entities.UserResponse{}, web.WebError{Code: 400, Message: "The requested ID doesn't match with any record"}
+	}
+
+	// Konversi dari request ke domain entities user - mengabaikan nilai kosong pada request
+	copier.CopyWithOption(&user, &userRequest, copier.Option{IgnoreEmpty: true, DeepCopy: true})
+
+	// Hanya hash password jika password juga diganti (tidak kosong)
+	if userRequest.Password != "" {
+		hashedPassword, _ := helpers.HashPassword(user.Password)
+		user.Password = hashedPassword
+	}
+
+	// Update via repository
+	user, err = service.userRepo.Update(user)
+	// Konversi user domain menjadi user response
+	userRes := entities.UserResponse{}
+	copier.Copy(&userRes, &user)
+
+	return userRes, err
+}
+
+func (service UserService) DeleteUser(id int) error {
+
+	// Cari user berdasarkan ID via repo
+	_, err := service.userRepo.Find(id)
+	if err != nil {
+		return web.WebError{Code: 400, Message: "The request ID has been deleted or not exist"}
+	}
+
+	// Delete via repository
+	err = service.userRepo.Delete(id)
+	return err
+}
